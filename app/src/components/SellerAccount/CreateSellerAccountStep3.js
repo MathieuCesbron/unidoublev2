c5fd3012cbf783f121101dd2d6f2a018ed0f424f
Mathieu Cesbron <mathieu.cesbron@protonmail.com>
2023-02-06 18:55:14 +0100

    Add text for creating seller account step 3

--- app/src/components/SellerAccount/CreateSellerAccountStep3.js
+++ app/src/components/SellerAccount/CreateSellerAccountStep3.js
@@ -1,8 +1,19 @@
+import solanaLogo from "../../images/solana-logo.png";
+
 const CreateSellerAccountStep3 = () => {
   return (
     <div>
-      <h2>Create Seller Account</h2>
-      <button>Create Seller Account</button>
+      <h2 className="step-title">Create Seller Account</h2>
+      <p>
+        This is the last step, your account will be registred on Solana. you
+        will be able to list items and hopefully make sales after that !
+      </p>
+      <h3 className="total-cost">Cost: </h3>
+      <p className="cost">
+        <img className="token-logo" src={solanaLogo} alt="solana token logo" />
+        0.001 SOL
+      </p>
+      <button className="step-btn">Approve transaction on wallet</button>
     </div>
   );
 };
