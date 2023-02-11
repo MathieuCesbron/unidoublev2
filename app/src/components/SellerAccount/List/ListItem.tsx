9cb2127c8d74a841fd59f26a1fe9215c7501afe2
Mathieu Cesbron <mathieu.cesbron@protonmail.com>
2023-02-11 11:11:03 +0100

    Success uploading to shadow drive

--- app/src/components/SellerAccount/List/ListItem.tsx
+++ app/src/components/SellerAccount/List/ListItem.tsx
@@ -5,15 +5,16 @@ import {
   storePubKey,
 } from "../../../utils/solana/program";
 import * as anchor from "@project-serum/anchor";
-import { useAnchorWallet, useWallet } from "@solana/wallet-adapter-react";
+import { AnchorWallet, useWallet } from "@solana/wallet-adapter-react";
 import { IoArrowBackCircleOutline } from "react-icons/io5";
 import "./ListItem.css";
 import {
   getDecodedSellerAccount,
   getSellerAccount,
 } from "../../../utils/solana/sellerAccount";
-import { ShdwDrive } from "@shadow-drive/sdk";
+import { ShadowFile, ShdwDrive } from "@shadow-drive/sdk";
 import { PublicKey } from "@solana/web3.js";
+import { upload } from "@testing-library/user-event/dist/upload";
 
 interface Props {
   setMode: (s: string) => void;
@@ -24,10 +25,10 @@ type account = {
 };
 
 const ListItem = (props: Props) => {
-  const wallet = useAnchorWallet();
+  const wallet = useWallet();
   const { publicKey } = useWallet();
 
-  const [file, setFile] = useState<File>();
+  const [fileList, setFileList] = useState<any>();
   const [shdwHash, setShdwHash] = useState("");
   const [sellerAccount, setSellerAccount] = useState<account>();
   const [itemNumber, setItemNumber] = useState(-1);
@@ -56,52 +57,63 @@ const ListItem = (props: Props) => {
   const listItemHandler = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    console.log(file);
     // Upload data on shadow drive if we are on mainnet.
-    const drive = await new ShdwDrive(privateConnection, wallet).init();
+
+    const files: ShadowFile[] = [
+      { name: "test.json", file: Buffer.from(JSON.stringify({ ama: "test" })) },
+    ];
 
     try {
-      const uploadFile = await drive.uploadFile(
+      const drive = await new ShdwDrive(
+        privateConnection,
+        wallet as AnchorWallet,
+      ).init();
+
+      const uploadMultipleFiles = await drive.uploadMultipleFiles(
         new PublicKey(shdwHash),
-        new File(["{oula: 'test'}"], "test.json"),
-        // file!,
-        "v2",
+        fileList as FileList,
       );
-      console.log(uploadFile);
+      console.log(uploadMultipleFiles);
     } catch (error) {
       console.log(error);
     }
 
-    const [item] = anchor.web3.PublicKey.findProgramAddressSync(
-      [
-        publicKey!.toBuffer(),
-        new anchor.BN(itemNumber).toArrayLike(Buffer, "le", 2),
-      ],
-      program.programId,
-    );
+    // const [item] = anchor.web3.PublicKey.findProgramAddressSync(
+    //   [
+    //     publicKey!.toBuffer(),
+    //     new anchor.BN(itemNumber).toArrayLike(Buffer, "le", 2),
+    //   ],
+    //   program.programId,
+    // );
 
-    try {
-      const txListItem = await program.methods
-        .listItem(
-          itemFormData.category,
-          itemFormData.price,
-          itemFormData.amount,
-        )
-        .accounts({
-          user: publicKey!,
-          store: storePubKey,
-          sellerAccount: sellerAccount?.pubkey,
-          item: item,
-        })
-        .rpc();
-      console.log("tx list item: ", txListItem);
-    } catch (error) {
-      console.log(error);
-    }
+    // try {
+    //   const txListItem = await program.methods
+    //     .listItem(
+    //       itemFormData.category,
+    //       itemFormData.price,
+    //       itemFormData.amount,
+    //     )
+    //     .accounts({
+    //       user: publicKey!,
+    //       store: storePubKey,
+    //       sellerAccount: sellerAccount?.pubkey,
+    //       item: item,
+    //     })
+    //     .rpc();
+    //   console.log("tx list item: ", txListItem);
+    // } catch (error) {
+    //   console.log(error);
+    // }
   };
 
   return (
     <div className="list-wrapper">
+      <input
+        type={"file"}
+        onChange={(e) => {
+          setFileList(e.target.files);
+        }}
+      ></input>
       <div className="list-top">
         <IoArrowBackCircleOutline
           className="back-arrow"
@@ -116,9 +128,14 @@ const ListItem = (props: Props) => {
           <input className="input-title"></input>
         </div>
         <div className="list-form-field">
+          <label>Description</label>
+          <input className="input-description"></input>
+        </div>
+        <div className="list-form-field">
           <label>Private key</label>
           <input className="input-private-key" type="password"></input>
         </div>
+
         <button disabled={loading} className="list-btn" type="submit">
           Validate transaction on wallet
         </button>
