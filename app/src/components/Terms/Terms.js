2c17f2dcded5c8f9d34d2c639665ccc3b86029fd
Mathieu Cesbron <mathieu.cesbron@protonmail.com>
2023-02-23 13:17:50 +0100

    Add terms of use page

--- app/src/components/Terms/Terms.js
+++ app/src/components/Terms/Terms.js
@@ -1,5 +1,23 @@
+import { AiOutlineHome } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+import "../SellerAccount/Option.css";
+import "./Terms.css";
+
 const Terms = () => {
-  return <>terms</>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="option-wrapper">
+      <div className="option-top" style={{ justifyContent: "center" }}>
+        <AiOutlineHome
+          size={"2em"}
+          className="home-logo"
+          onClick={() => navigate("/")}
+        />
+        <h2>Terms of use coming soon</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Terms;
